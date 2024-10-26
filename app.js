141ab796bbb2e6891fa6ca1c3a738ce019f592d8
Goran7777 <goranilicilke@yahoo.com>
2024-10-26 14:45:20 +0200

    dodata metoda provere karaktera

--- app.js
+++ app.js
@@ -22,5 +22,28 @@ Slagalica.prototype.vratiSlagalicu = function () {
   return slagalica;
 };
 
+Slagalica.prototype.napraviPokusaj = function (pokusaj) {
+  pokusaj = pokusaj.toLowerCase();
+  const jelJedinstven = !this.pokusanaSlova.includes(pokusaj);
+  const jelLosPokusaj = !this.rec.includes(pokusaj);
+  if (jelJedinstven) {
+    this.pokusanaSlova.push(pokusaj);
+  }
+  if (!jelJedinstven) {
+    alert.textContent = "Dupliras slovo!";
+    setInterval(function () {
+      alert.textContent = "";
+    }, 2000);
+  }
+  if (jelJedinstven && jelLosPokusaj) {
+    if (!this.ostaloPokusaja <= 0) {
+      this.ostaloPokusaja--;
+    }
+  }
+  if (this.ostaloPokusaja === 0) {
+    izgubio.textContent = "Izgubili ste igru,osvezite stranu za novu!";
+  }
+};
+
 const igra1 = new Slagalica("vrabac", 5);
 recHtml.textContent = igra1.vratiSlagalicu();
