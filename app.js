849907c82d3b5cab5104a9a7776b74f5a15c1ac3
Goran7777 <goranilicilke@yahoo.com>
2024-10-26 14:14:55 +0200

    dodana metoda na prototip

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+let recHtml = document.getElementById("rec");
+let pokusajiHtml = document.getElementById("pokusaji");
+let alert = document.getElementById("alert");
+let izgubio = document.getElementById("rec");
 
+const Slagalica = function (rec, ostaloPokusaja) {
+  this.rec = rec.toLowerCase().split("");
+  this.ostaloPokusaja = ostaloPokusaja;
+  this.pokusanaSlova = [];
+};
+
+Slagalica.prototype.vratiSlagalicu = function () {
+  let slagalica = "";
+  this.rec.forEach((slovo) => {
+    if (this.pokusanaSlova.includes(slovo) || slovo === " ") {
+      slagalica += slovo;
+    } else {
+      slagalica += "*";
+    }
+  });
+
+  return slagalica;
+};
+
+const igra1 = new Slagalica("vrabac", 5);
+recHtml.textContent = igra1.vratiSlagalicu();
