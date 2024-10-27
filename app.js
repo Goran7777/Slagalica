fa131d0a2d6d4e9dff4e45eb1c7c835a6bde0776
Goran7777 <goranilicilke@yahoo.com>
2024-10-27 12:22:21 +0100

    logika promenjena,dodat status igre

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 let recHtml = document.getElementById("rec");
-let izgubio = document.getElementById("izgubio");
-let pokusajiHtml = document.getElementById("pokusaji");
-let alert = document.getElementById("alert");
-let pobeda = document.getElementById("pobeda");
+let poruka = document.getElementById("poruka");
 
 const Slagalica = function (rec, ostaloPokusaja) {
   this.rec = rec.toLowerCase().split("");
   this.ostaloPokusaja = ostaloPokusaja;
   this.pokusanaSlova = [];
+  this.status = "u-toku";
+};
+
+Slagalica.prototype.izracunajStatus = function () {
+  const zavrsenoUspesno = this.rec.every((slovo) =>
+    this.pokusanaSlova.includes(slovo)
+  );
+  if (this.ostaloPokusaja === 0) {
+    this.status = "neuspeh";
+  } else if (zavrsenoUspesno) {
+    this.status = "uspeh";
+  } else {
+    this.status = "u-toku";
+  }
+};
+
+Slagalica.prototype.vratiStatusPoruke = function () {
+  if (this.status === "u-toku") {
+    return `Ostalo pokusaja: ${this.ostaloPokusaja}`;
+  } else if (this.status === "neuspeh") {
+    return `Dobar pokusaj!Rec je bila ${this.rec.join("")}.`;
+  } else {
+    return "Sjajno!Pogodili ste zadatu rec ! ! !";
+  }
 };
 
 Slagalica.prototype.vratiSlagalicu = function () {
@@ -24,48 +45,27 @@ Slagalica.prototype.vratiSlagalicu = function () {
 };
 
 Slagalica.prototype.napraviPokusaj = function (pokusaj) {
-  const uspesnoZavrseno = this.rec.every((slovo) =>
-    this.pokusanaSlova.includes(slovo)
-  );
-  console.log(uspesnoZavrseno);
-
   pokusaj = pokusaj.toLowerCase();
   const jelJedinstven = !this.pokusanaSlova.includes(pokusaj);
   const jelLosPokusaj = !this.rec.includes(pokusaj);
+  if (this.status !== "u-toku") {
+    return;
+  }
   if (jelJedinstven) {
     this.pokusanaSlova.push(pokusaj);
   }
-  if (!jelJedinstven) {
-    alert.textContent = "Dupliras slovo!";
-    setInterval(function () {
-      alert.textContent = "";
-    }, 2000);
-  }
-
   if (jelJedinstven && jelLosPokusaj) {
-    if (!this.ostaloPokusaja <= 0) {
-      this.ostaloPokusaja--;
-    } else {
-      return;
-    }
+    this.ostaloPokusaja--;
   }
-  if (uspesnoZavrseno) {
-    pobeda.textContent = "Cestitamo!Pogodili ste zadatu rec! ! !";
-  }
-  if (this.ostaloPokusaja === 0) {
-    izgubio.textContent = "Izgubili ste igru,osvezite stranu,pokusajte ponovo!";
-  }
-  console.log(this.pokusanaSlova);
+  this.izracunajStatus();
 };
 
 const igra1 = new Slagalica("tri", 2);
 recHtml.textContent = igra1.vratiSlagalicu();
-pokusajiHtml.textContent = `Ostalo pokusaja: ${igra1.ostaloPokusaja}`;
 window.addEventListener("keypress", function (e) {
   let recHtml = document.getElementById("rec");
-  let pokusajHtml = document.getElementById("pokusaji");
   const pokusaj = String.fromCharCode(e.charCode);
   igra1.napraviPokusaj(pokusaj);
   recHtml.textContent = igra1.vratiSlagalicu();
-  pokusajHtml.textContent = `Ostalo pokusaja: ${igra1.ostaloPokusaja}`;
+  poruka.textContent = igra1.vratiStatusPoruke();
 });
